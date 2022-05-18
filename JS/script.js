d51b2e6cd22eb9032e98973ee2abad0efc0e62a4
Ferdaus Zaman Polok <ferdaus15-9924@diu.edu.bd>
2022-05-18 11:53:46 +0600

    Show User Profile

--- JS/script.js
+++ JS/script.js
@@ -1,24 +1,26 @@
 let searchBtn = document.querySelector('#searchBtn');
 let searchUser = document.querySelector('#searchUser');
+let ui = new UI();
 
-searchBtn.addEventListener('click', (e)=>{
-    let userText = searchUser.ariaValueMax;
+searchBtn.addEventListener('click', (e) => {
+    let userText = searchUser.value;
 
-    if (userText != ''){
+    if (userText != '') {
         //Fetch API
 
         fetch(`https://api.github.com/users/${userText}`)
-        .then(result => result.json())
-        .then(data =>{
-            if(data.message =='Not Found'){
-                //show alert
-            }else{
-                //show profile
-            }
-        })
+            .then(result => result.json())
+            .then(data => {
+                if (data.message == 'Not Found') {
+                    //show alert
+                } else {
+                    //show profile
+                    ui.showProfile(data)
+                }
+            })
 
-    }else{
+    } else {
         //Clear Profile
-        
+
     }
 })
